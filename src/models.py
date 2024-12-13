8eaac7cc7a49b3ef20c8b2cba06601f50acc88f2
ViesK <109549664+ViesK@users.noreply.github.com>
2024-12-13 00:08:04 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Followers(Base):
+    __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    #Creamos claves foraneas
+    from_id = Column(Integer, ForeignKey('users.id'))
+    to_id = Column(Integer, ForeignKey('users.id'))
+    #Conectamos la tabla con Post (N-N)
+    user_from_id = relationship('Users', foreign_keys=[from_id], backref='Follower')
+    user_to_id = relationship('Users', foreign_keys=[to_id], backref='Followed')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    email = Column(String, nullable=False, unique=True)  # No puede estar vacio (nullable) y debe ser unico (unique)
+    password = Column(String, nullable=False)
+    city = Column(String)
 
-    def to_dict(self):
-        return {}
+    
+class Posts(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    content = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    #Conectamos la tabla con User (1-N)
+    user = relationship('Users', backref='posts')
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    content = Column(String, nullable=False)
+    #Creamos claves foraneas
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    #Conectamos la tabla con Post (1-N)
+    user = relationship('Users', backref='comments')
+    post = relationship('Posts', backref='comments')
+
+
+class Medias(Base):
+    __tablename__ = 'medias'
+    id = Column(Integer, primary_key=True)
+    src = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    #Conectamos la tabla con Post (1-N)
+    post = relationship('Posts', backref='medias')
+
 
 ## Draw from SQLAlchemy base
 try:
